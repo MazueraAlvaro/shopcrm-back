54423b7c45aa1c1b44799a26d5dacc4788b6352d
Alvaro Mazuera U <me@alvaromazuera.com>
2022-06-21 13:35:58 -0500

    :construction: Add nestjs config module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/config')
+  getConfig(): number {
+    return this.configService.get<number>('PORT');
   }
 }
